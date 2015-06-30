b4616e953e68a8147b69b6b64fb3d2c35ef0cca6
hhzl <hannes.hirzel@gmail.com>
2015-06-30 06:02:53 +0000

    deploy

--- deploy.js
+++
@@ -1,9 +0,0 @@
-define([
-    'amber/lang',
-    // --- packages to be deployed begin here ---
-    'amber/legacy/IDE',
-    'amber-trysmalltalk/TrySmalltalk'
-    // --- packages to be deployed end here ---
-], function (amber) {
-    return amber;
-});
